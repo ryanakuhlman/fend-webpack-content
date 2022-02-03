25214454eb6828e888bf922c1297eb67edc7bbb6
Ryan <ryanakuhlman@gmail.com>
2022-02-03 11:19:21 +1100

    updated the code

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,5 @@
-import { handleSubmit } from './js/formHandler'
-import { checkForName } from './js/nameChecker'
+import { handleSubmit } from './js/formHandler.js'
+import { checkForName } from './js/nameChecker.js'
 
 alert("I exist");
 console.log("why does it not show?");
